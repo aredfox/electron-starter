7bef6dcb8bcfe666dfdccf7066c5c941d26a8042
Yves Schelpe <yves.Schelpe@kdg.be>
2017-01-28 23:09:00 +0100

    added top leve clean task

---
+++ gulp/tasks/clean.js
@@ -0,0 +1,11 @@
+/***************************************************************************
+ * Created by Yves Schelpe (ys@delegate-it.be) for Delegate-IT.be
+ *         at Sat Jan 28 2017 23:07:48
+ * Copyright (c) 2017 Delegate-IT.be
+ ***************************************************************************/
+
+// Task that runs the full clean cycle.
+
+module.exports = {
+    dep: ['clean:icons', 'clean:compilation']
+};
