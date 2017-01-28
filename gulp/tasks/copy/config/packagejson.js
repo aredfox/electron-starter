0079b587b8a0488a64aeca57da55193368c95e45
Yves Schelpe <yves.Schelpe@kdg.be>
2017-01-28 20:46:28 +0100

    Added task to strip package.json for production

---
+++ gulp/tasks/copy/config/packagejson.js
@@ -0,0 +1,29 @@
+/***************************************************************************
+ * Created by Yves Schelpe (ys@delegate-it.be) for Delegate-IT.be
+ *         at Sat Jan 28 2017 20:36:30
+ * Copyright (c) 2017 Delegate-IT.be
+ ***************************************************************************/
+
+// Task that prepares and copies over package.json
+// for the packaged application
+
+module.exports = {
+    dep: [],
+    fn: function (gulp, callback, plugins) {
+        return gulp
+            .src(plugins.config.paths.packagejson)
+            .pipe(plugins.jsonEditor(file => {
+                file.main = "main";
+                delete file.scripts;
+                delete file.devDependencies;
+                delete file.build;
+                delete file.directories;
+                delete file.repository;
+                return file;
+            }, {
+                'indent_char': '\t',
+                'indent_size': 1
+            }))
+            .pipe(gulp.dest(plugins.config.paths.destination.base));
+    }
+};
